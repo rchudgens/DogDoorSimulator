1071903835b793ca2a5dfc1b3fb334a70e906b9f
Ronnie Hudgens <ronnie.hudgens@bellcounty.texas.gov>
2026-03-19 17:04:37 -0500

    Create DogDoor.cs
    Added DogDoor.cs

---
+++ DogDoorSimulator/Models/DogDoor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DogDoorSimulator.Models
+{
+    internal class DogDoor
+    {
+        public bool IsOpen { get; set; }
+
+        public DogDoor()
+        {
+            IsOpen = false;
+        }
+
+        public void Open()
+        {
+            Console.WriteLine("The dog door opens.");
+            IsOpen = true;
+        }
+        public void Close()
+        {
+            Console.WriteLine("The dog door closes.");
+            IsOpen = false;
+        }
+    }
+}
