86309a8c5732873ef6f704f20c690094d7ce3cae
Ronnie Hudgens <ronnie.hudgens@bellcounty.texas.gov>
2026-03-19 11:06:25 -0500

    Initial commit. Forgot to add files before doing previous commit.

---
+++ DogDoorSimulator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DogDoorSimulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
