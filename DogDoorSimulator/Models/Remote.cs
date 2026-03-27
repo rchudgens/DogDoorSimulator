b8af74a88c7edeba5971fe491cdddd3f516e9cce
Ronnie Hudgens <ronnie.hudgens@bellcounty.texas.gov>
2026-03-27 17:06:26 -0500

    Update test scenario to let Fido back inside.
    Since door is only open 5 seconds, the door will have to be opened again to let Fido back inside. Added code to do this.

--- DogDoorSimulator/Models/Remote.cs
+++ DogDoorSimulator/Models/Remote.cs
@@ -24,7 +24,8 @@ namespace DogDoorSimulator.Models
             else
             {
                 _door.Open();
-                await Task.Delay(5000); // Simulate the dog going through the door
+                //await Task.Delay(5000); // Simulate the dog going through the door
+                Thread.Sleep(5000); // Simulate the dog going through the door
                 _door.Close();
                 Console.WriteLine("The dog has gone through the door and it is now closed.");
             }
--- DogDoorSimulator/Program.cs
+++ DogDoorSimulator/Program.cs
@@ -9,8 +9,14 @@ namespace DogDoorSimulator
             DogDoor door = new DogDoor();
             Remote remote = new Remote(door);
             
+            Console.WriteLine("Fido barks to go outside...");
+            Console.WriteLine("Fido's owner presses the remote control button...");
             remote.PressButton();
-            //remote.PressButton();
+            Console.WriteLine("Fido has gone outside...");
+            Console.WriteLine("Now Fido is done and barks to come back inside...");
+            Console.WriteLine("Fido's owner presses the remote control button...");
+            remote.PressButton();
+            Console.WriteLine("Fido is back inside...");
 
             Console.ReadLine();
         }
